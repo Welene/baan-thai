579a5d955ed07f8fe522357d57598c968fb00530
Sunsanee Persson <3kdyhfe24.perssu@folkuniversitetet.nu>
2025-12-04 09:27:02 +0100

    Add link to ArbetsVy in AdminNavBar

--- packages/baan-thai-frontend/src/components/AdminNavBar/AdminNavBar.tsx
+++ packages/baan-thai-frontend/src/components/AdminNavBar/AdminNavBar.tsx
@@ -7,6 +7,7 @@ export const AdminNavBar = () => {
 			<div className="admin-nav-content">
 				<h2>Admin Panel</h2>
 				<div className="admin-nav-links">
+                    <Link to="/admin/menu">ArbetsVy</Link>
 					<Link to="/admin/menu">Skapa ny</Link>
 					<Link to="/admin/menu/edit">Redigera</Link>
 					<Link to="/landing">Till huvudsida</Link>
