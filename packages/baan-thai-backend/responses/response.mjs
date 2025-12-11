93322d2b4a2580655f8114c0f1c2a58b44b68199
Tivva34 <tivva91@gmail.com>
2025-12-11 14:17:50 +0100

    Added Csp to Html as meta and added it to response.mjs now it should be used in both frontend and backend requests

--- packages/baan-thai-backend/responses/response.mjs
+++ packages/baan-thai-backend/responses/response.mjs
@@ -1,6 +1,22 @@
+const CSP = "default-src 'none'; " +
+  "base-uri 'self'; " +
+  "script-src 'self'; " +
+  "style-src 'self' 'unsafe-inline'; " +
+  "img-src 'self' data: https://baan-thai-bucket.s3-website.eu-north-1.amazonaws.com; " +
+  "font-src 'self' data:; " +
+  "connect-src 'self' https://nicx8149f2.execute-api.eu-north-1.amazonaws.com; " +
+  "object-src 'none'; " +
+  "form-action 'self'; " +
+  "frame-ancestors 'none'; " +
+  "upgrade-insecure-requests;";
+
 export const sendResponse = (code, data) => {
   return {
     statusCode: code,
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Security-Policy': CSP
+    },
     body: JSON.stringify({
       ...data,
     }),
